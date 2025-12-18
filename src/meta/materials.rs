e55ed45869d4ae635f693c907fa540aa2dc40c70
Jordan Hendl <jordiehendl@gmail.com>
2025-12-18 15:38:24 -0600

    Rework materials for furikake and remove sample shaders

--- src/meta/materials.rs
+++ src/meta/materials.rs
@@ -1,22 +1,55 @@
-use crate::meta::{DeviceTexture, DeviceTextureList, GraphicsShader, HostTexture};
+use crate::meta::{DeviceTexture, DeviceTextureList, HostTexture};
+use furikake::types::Material as FurikakeMaterial;
+use std::fmt;
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct HostMaterial {
     pub name: String,
     pub textures: Vec<HostTexture>,
-    pub shader: Option<GraphicsShader>,
+    pub material: FurikakeMaterial,
 }
 
 #[repr(C)]
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Default)]
 pub struct DeviceMaterial {
     pub textures: DeviceTextureList,
-    pub shader: Option<GraphicsShader>,
+    pub material: FurikakeMaterial,
+}
+
+impl fmt::Debug for HostMaterial {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("HostMaterial")
+            .field("name", &self.name)
+            .field("textures", &self.textures)
+            .field("material", &format_material(&self.material))
+            .finish()
+    }
+}
+
+impl fmt::Debug for DeviceMaterial {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("DeviceMaterial")
+            .field("textures", &self.textures)
+            .field("material", &format_material(&self.material))
+            .finish()
+    }
+}
+
+fn format_material(material: &FurikakeMaterial) -> String {
+    format!(
+        "{{ base_color_texture_id: {}, normal_texture_id: {}, metallic_roughness_texture_id: {}, occlusion_texture_id: {}, emissive_texture_id: {}, render_mask: {} }}",
+        material.base_color_texture_id,
+        material.normal_texture_id,
+        material.metallic_roughness_texture_id,
+        material.occlusion_texture_id,
+        material.emissive_texture_id,
+        material.render_mask
+    )
 }
 
 impl DeviceMaterial {
-    /// Builds a material from the provided textures and optional graphics shader.
-    pub fn new(textures: Vec<DeviceTexture>, shader: Option<GraphicsShader>) -> Self {
+    /// Builds a material from the provided textures and furikake material definition.
+    pub fn new(textures: Vec<DeviceTexture>, material: FurikakeMaterial) -> Self {
         let mut list = DeviceTextureList::new();
         for texture in textures
             .into_iter()
@@ -27,7 +60,7 @@ impl DeviceMaterial {
         debug_assert!(list.len() <= super::textures::DEVICE_TEXTURE_CAPACITY);
         Self {
             textures: list,
-            shader,
+            material,
         }
     }
 }
